14e84d48ab4275cf293a054367c5c61d46f8f1a7
Obspogon <72287594+Obspogon@users.noreply.github.com>
2025-05-21 13:47:49 -0400

    add albums

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Hello!</Text>
+			<Text style={[styles.header, styles.text]}>Albums</Text>
+			<View style={styles.albumLayout}>
+				<View style={{ width: "100%", flex: 1 }}>
+					<Image style={styles.albumImage} source={require("./assets/album.png")} />
+					<Text style={styles.text}>Album name</Text>
+				</View>
+				<View style={{ width: "100%", flex: 1 }}>
+					<Image style={styles.albumImage} source={require("./assets/album.png")} />
+					<Text style={styles.text}>Album name</Text>
+				</View>
+				<View style={{ width: "100%", flex: 1 }}>
+					<Image style={styles.albumImage} source={require("./assets/album.png")} />
+					<Text style={styles.text}>Album name</Text>
+				</View>
+				<View style={{ width: "100%", flex: 1 }}>
+					<Image style={styles.albumImage} source={require("./assets/album.png")} />
+					<Text style={styles.text}>Album name</Text>
+				</View>
+			</View>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -12,9 +30,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
 		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
+		padding: 50,
 	},
+	header: { fontSize: 20, fontWeight: 500 },
+	text: { fontSize: 15 },
+	albumImage: { height: 100, width: 100, borderWidth: 2, borderColor: "#000" },
+	albumLayout: { flexDirection: "row", flexWrap: "wrap" },
 });
