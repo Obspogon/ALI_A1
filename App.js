b4440bac96056992c9a4eaeb8d238a3a193bb8a5
Obspogon <72287594+Obspogon@users.noreply.github.com>
2025-05-23 14:24:26 -0400

    fix scrolling

--- App.js
+++ App.js
@@ -7,42 +7,44 @@ import AudiobookComponent from "./AudiobookComponent";
 
 export default function App() {
 	return (
-		<ScrollView style={styles.container}>
-			<View style={styles.menuBar}>
-				<MenuComponent text="All" />
-				<MenuComponent text="Mixes" />
-				<MenuComponent text="Albums" />
-				<MenuComponent text="Audiobooks" />
-			</View>
-			<Text style={[styles.text, styles.header]}>Mixes</Text>
-			<View style={styles.mixLayout}>
-				<MixComponent name="For You" />
-				<MixComponent name="Trending" />
-				<MixComponent name="Friends" />
-				<MixComponent name="Rock" />
-				<MixComponent name="Pop Punk" />
-				<MixComponent name="OST" />
-			</View>
-			<Text style={[styles.text, styles.header]}>Albums</Text>
-			<View style={styles.albumLayout}>
-				<AlbumComponent name="Underta..." />
-				<AlbumComponent name="Bad" />
-				<AlbumComponent name="In The End" />
-				<AlbumComponent name="The Killers" />
-				<AlbumComponent name="Deltaru..." />
-				<AlbumComponent name="Help" />
-			</View>
-			<Text style={[styles.text, styles.header]}>Audiobooks</Text>
-			<View style={styles.albumLayout}>
-				<AudiobookComponent name="Sherlock" />
-				<AudiobookComponent name="Playing with..." />
-				<AudiobookComponent name="In The End" />
-				<AudiobookComponent name="Pride &..." />
-				<AudiobookComponent name="What Happ..." />
-				<AudiobookComponent name="How to..." />
-			</View>
-			<StatusBar style="auto" />
-		</ScrollView>
+		<View style={styles.container}>
+			<ScrollView>
+				<View style={styles.menuBar}>
+					<MenuComponent text="All" />
+					<MenuComponent text="Mixes" />
+					<MenuComponent text="Albums" />
+					<MenuComponent text="Audiobooks" />
+				</View>
+				<Text style={[styles.text, styles.header]}>Mixes</Text>
+				<View style={styles.mixLayout}>
+					<MixComponent name="For You" />
+					<MixComponent name="Trending" />
+					<MixComponent name="Friends" />
+					<MixComponent name="Rock" />
+					<MixComponent name="Pop Punk" />
+					<MixComponent name="OST" />
+				</View>
+				<Text style={[styles.text, styles.header]}>Albums</Text>
+				<View style={styles.albumLayout}>
+					<AlbumComponent name="Underta..." />
+					<AlbumComponent name="Bad" />
+					<AlbumComponent name="In The End" />
+					<AlbumComponent name="The Killers" />
+					<AlbumComponent name="Deltaru..." />
+					<AlbumComponent name="Help" />
+				</View>
+				<Text style={[styles.text, styles.header]}>Audiobooks</Text>
+				<View style={styles.albumLayout}>
+					<AudiobookComponent name="Sherlock" />
+					<AudiobookComponent name="Playing with..." />
+					<AudiobookComponent name="In The End" />
+					<AudiobookComponent name="Pride &..." />
+					<AudiobookComponent name="What Happ..." />
+					<AudiobookComponent name="How to..." />
+				</View>
+				<StatusBar style="auto" />
+			</ScrollView>
+		</View>
 	);
 }
 
@@ -50,13 +52,11 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#000",
-		paddingTop: 50,
-		paddingLeft: 50,
-		paddingRight: 50,
-		paddingBottom: 500,
+		padding: 30,
 	},
 	header: {
 		fontSize: 20,
+		margin: 5,
 		fontStyle: "bold",
 	},
 	text: { fontSize: 15, color: "#fff" },
